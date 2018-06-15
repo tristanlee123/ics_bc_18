4c9cd81a864e1500837fbd9837c8bc090241cf8c
tristanlee123 <tristanasdf5809@gmail.com>
2018-06-14 20:54:32 -0700

    Complete bigger_better_favorite_number exercise

--- week1/ch05/bigger_better_favorite_number.rb
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -1,8 +1,10 @@
 #bigger better favorite number
 puts 'Hello, I would like get to know you better, so let me ask you a couple of questions'
 puts 'What is your favorite color?'
+$stdout.flush
 color = gets.chomp
 puts 'what is your favorite number?'
+$stdout.flush
 favorite_number = gets.chomp.to_i
 better_number = favorite_number + 1
 puts 'But don\'t you think ' + better_number.to_s + ' is a better number?'
