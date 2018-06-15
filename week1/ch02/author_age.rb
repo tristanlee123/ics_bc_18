4a0e318ebb0fb8f0fcab9843e702ee3c0fb5bcc8
tristanlee123 <tristanasdf5809@gmail.com>
2018-06-14 19:01:42 -0700

    Complete author_age exercise

---
+++ week1/ch02/author_age.rb
@@ -0,0 +1,9 @@
+#authors age
+seconds_author = 11.6 * 10**8
+minutes = (seconds_author) / 60
+hours = (minutes) / 60
+days = (hours) / 24
+year = (days) / 365.25
+puts 'the author is'
+puts year
+puts 'years old'
