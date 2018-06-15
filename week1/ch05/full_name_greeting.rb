b07cde40ba1c04f447d5363b6445c5b0bcc6cb4c
tristanlee123 <tristanasdf5809@gmail.com>
2018-06-14 20:53:21 -0700

    Complete full_name_greeting exercise

--- week1/ch05/full_name_greeting.rb
+++ week1/ch05/full_name_greeting.rb
@@ -1,10 +1,13 @@
 #full name greeting
 puts 'Hello my name is Tristan Anthony Villanueva'
 puts 'What\'s your first name?'
+$stdout.flush
 first_name = gets.chomp
 puts 'Alright so your first name is ' + first_name
 puts 'Now, what is your middle name?'
+$stdout.flush
 middle_name = gets.chomp
 puts middle_name + ' Ok, now what is your last name?'
+$stdout.flush
 last_name= gets.chomp
 puts 'Nice to meet you ' + first_name + ' ' + middle_name + ' ' + last_name + '!'
