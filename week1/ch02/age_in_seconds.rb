1af5399841742018a17e797cb4f1afdef9b99767
tristanlee123 <tristanasdf5809@gmail.com>
2018-06-14 19:00:36 -0700

    Complete age_in_seconds exercise

---
+++ week1/ch02/age_in_seconds.rb
@@ -0,0 +1,10 @@
+#age in seconds
+second = 1
+minute = (second) * 60
+hour = (minute) * 60
+day = (hour) * 24
+year = (day) * 365.25
+my_age = (year) * 17
+puts 'I am'
+puts my_age
+puts 'seconds old'
