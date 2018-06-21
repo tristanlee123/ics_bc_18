22d3a0db7601dcccb9bd78cfa2f72970dd6229c3
tristanlee123 <tristanasdf5809@gmail.com>
2018-06-20 17:19:37 -0700

    Complete angry_boss exercise

---
+++ week2/ch06/angry_boss.rb
@@ -0,0 +1,6 @@
+#angryboss exercise
+ puts 'You are an chef at a restaurant where your boss pays you very little, you work every day full time, and you are not allowed to take a day off'
+ puts 'So you walk up to your boss'
+ puts 'What do you demand?'
+ demand = gets.chomp
+ puts 'Boss responds: ' + '"' + demand.upcase + '" GET OUT OF HERE,YOU\'RE FIRED!!!!!'
