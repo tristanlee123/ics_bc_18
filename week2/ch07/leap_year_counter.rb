7e393e9efe15843215fdd3322dd2beb591311256
tristanlee123 <tristanasdf5809@gmail.com>
2018-06-21 19:58:40 -0700

    Complete leap_year_counter exercise

---
+++ week2/ch07/leap_year_counter.rb
@@ -0,0 +1,15 @@
+#counter
+puts 'Chose your starting year'
+year_range_start = gets.chomp.to_i
+puts 'Alright now chose your ending year'
+year_range_end = gets.chomp.to_i
+puts 'Ok so your year range is from ' + year_range_start.to_s + ' to ' + year_range_end.to_s
+puts 'These are your leap years!'
+while year_range_start <= year_range_end
+  if year_range_start%4 == 0
+  	if  year_range_start%100 != 0 || year_range_start%400 == 0
+      puts year_range_start
+    end
+  end
+  year_range_start = year_range_start + 1
+end
