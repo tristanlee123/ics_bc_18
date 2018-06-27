43566c9457faab786dd49cbf66726835c5751329
tristanlee123 <tristanasdf5809@gmail.com>
2018-06-26 22:51:35 -0700

    Complete old_roman_numerals exercise

--- week3/ch09/old_roman_numerals.rb
+++ week3/ch09/old_roman_numerals.rb
@@ -6,7 +6,8 @@ def old_roman_numeral user_input
   roman_numeral = roman_numeral + 'D' * (user_input % 1000 / 500)
   roman_numeral = roman_numeral + 'C' * (user_input % 500 / 100)
   roman_numeral = roman_numeral + 'L' * (user_input % 100 / 50)
-  roman_numeral = roman_numeral + 'V' * (user_input % 50 / 5)
+  roman_numeral = roman_numeral + 'X' * (user_input % 50 / 10)
+  roman_numeral = roman_numeral + 'V' * (user_input % 10 / 5)
   roman_numeral = roman_numeral + 'I' * (user_input % 5 / 1)
 end
 puts (old_roman_numeral(user_input)) + ' is ' + user_input.to_s + ' in Roman numerals'
