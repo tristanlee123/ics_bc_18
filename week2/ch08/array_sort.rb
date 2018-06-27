d9f4eac5d59ef6697bb8a7f3e322fbb881f151d1
tristanlee123 <tristanasdf5809@gmail.com>
2018-06-26 21:53:28 -0700

    Improved and fixed style

--- week2/ch08/array_sort.rb
+++ week2/ch08/array_sort.rb
@@ -1,15 +1,14 @@
-#sorting words
 puts 'You can type in words you want to be sorted '
 puts 'When you are finished typing in your words type STOP to have your sorted list'
 list = []
 while true
-  puts 'Ok now another word'
   word = gets.chomp
+  puts 'Ok now another word'
   list.push word
-  if word =='STOP'
-  	list.pop
-  	puts 'Ok here is your list'
-    puts list.sort
-    break
+if word =='STOP'
+  list.pop
+  puts 'Ok here is your list'
+  puts list.sort
+  break
   end
 end
