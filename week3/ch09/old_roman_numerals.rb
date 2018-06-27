cd363aceaa3cba7d8bac0c05c50cdb0f184a21d5
tristanlee123 <tristanasdf5809@gmail.com>
2018-06-26 22:41:45 -0700

    Complete old_roman_numerals exercise

---
+++ week3/ch09/old_roman_numerals.rb
@@ -0,0 +1,12 @@
+#old roman numerals
+puts 'Type in the number you would like to translate to the old Roman Numeral'
+user_input = gets.chomp.to_i
+def old_roman_numeral user_input
+  roman_numeral = 'M' * (user_input / 1000)
+  roman_numeral = roman_numeral + 'D' * (user_input % 1000 / 500)
+  roman_numeral = roman_numeral + 'C' * (user_input % 500 / 100)
+  roman_numeral = roman_numeral + 'L' * (user_input % 100 / 50)
+  roman_numeral = roman_numeral + 'V' * (user_input % 50 / 5)
+  roman_numeral = roman_numeral + 'I' * (user_input % 5 / 1)
+end
+puts (old_roman_numeral(user_input)) + ' is ' + user_input.to_s + ' in Roman numerals'
