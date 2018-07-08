96f5a8e3fda410b5ceda8f9c868b3e59263a2928
tristanlee123 <tristanasdf5809@gmail.com>
2018-07-08 13:05:26 -0700

    Complete shuffle exercise

--- week4/ch10/sort.rb
+++ week4/ch10/sort.rb
@@ -3,7 +3,22 @@ def sort(some_array) # This "wraps" recursive_sort.
 end
 
 def recursive_sort(unsorted_array, sorted_array)
-  # Your fabulous code goes here.
-end
+  if unsorted_array.length <= 0
+  	return sorted_array
+  end
 
-sort [__, __, ...]
+  small = unsorted_array.pop
+  remaining_unsorted = []
+
+  unsorted_array.each do |tested_element|
+    if tested_element < small
+      remaining_unsorted.push small
+      small = tested_element
+    else
+      remaining_unsorted.push tested_element
+  end
+end
+  sorted_array.push small
+  recursive_sort remaining_unsorted, sorted_array
+end
+sort ['mexico', 'fish', 'lines', 'forest']
