61148b832dccf3dabae2187226de3a6ce5d5debf
tristanlee123 <tristanasdf5809@gmail.com>
2018-07-08 13:08:18 -0700

    Attempt at array_sum exercise

--- week4/recursion_practice/array_sum.rb
+++ week4/recursion_practice/array_sum.rb
@@ -1,9 +1,10 @@
+#THIS CODE IS NOT FINISHED THIS IS WHAT I HAVE SO FAR!!!
 def array_sum(array)
-  if ______________________
+  if array.length == 1
     array[0]
   else
-    ______________________
+    array[array.length - 1] + array_sum(array.pop) #This is the part where I am stuck
   end
 end
 
-puts array_sum [__, __, ...] 
+puts array_sum([4, 1, 8, 17, 14, 3])
