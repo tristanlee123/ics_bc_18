cb4af5aa523fdc7ea132589afdc6ee988fc6c0b3
tristanlee123 <tristanasdf5809@gmail.com>
2018-07-08 13:15:18 -0700

    Complete english_number exercise

--- week4/ch10/english_number.rb
+++ week4/ch10/english_number.rb
@@ -9,14 +9,20 @@ def english_number(number)
                   sixty seventy eighty ninety]
   teenagers = %w[eleven twelve thirteen
                  fourteen fifteen sixteen seventeen eighteen nineteen]
+  big_numbers = %w[[hundred, 2] [thousand, 3] [million, 6] [billion, 9] [trillion, 12] [quadrillion, 15] [quintillion, 18] [sextillion, 21] [septillion, 24] [octillion, 27] [nonillion, 30]
+   				         [decillion, 33] [undecillion, 36] [duodecillion, 39] [tredecillion, 42] [quattuordecillion, 45] [quindecillion, 48] [sexdecillion, 51] [septendecillion, 54] [octodecillion, 57] [novemdecillion, 60] [vigintillion, 63] [googol, 100]]
   # "left" is how much of the number
   # we still have left to write out.
   # "write" is the part we are
   # writing out right now.
   # write and left...get it? :)
   left = number
-  write = left / 100 # How many hundreds left?
-  left -= write * 100 # Subtract off those hundreds.
+  while big_numbers.length > 0
+    big_combination = big_numbers.pop
+    big_num_eng = big_combination[0]
+    big_base = 10 ** big_combination[1]
+    write = left / big_base
+    left -= write * big_base
   if write > 0
     # Now here's the recursion:
     hundreds = english_number write
@@ -26,6 +32,7 @@ def english_number(number)
       num_string += ' '
     end
   end
+end
   write = left / 10 # How many tens left?
   left -= write * 10 # Subtract off those tens.
   if write > 0
