bb20cdc471c7e51965f1b0f1781f591d2b2ec027
tristanlee123 <tristanasdf5809@gmail.com>
2018-07-13 12:11:20 -0700

    Complete pictures exercise

--- week5/ch11/pictures.rb
+++ week5/ch11/pictures.rb
@@ -1,38 +1,32 @@
-### In this next line you want to give it the file path of where you want the
-###  pictures moved. I recommend using an absolute filepath.
-###
-### An example for mac could be '/Users/username/Desktop/pic_dst'
-### An example for PC could be 'C:/Users/username/Desktop/pic_dst'
-Dir.chdir '<destination>'
 
-# First we find all of the pictures to be moved.
-### In the next line you want the source
-### Note the '/*.jpg' at the end, this is going to grab anything in the
-###  final folder that ends in '.jpg' and stash it in an array.
-###
-### An example for mac could be '/Users/username/Desktop/pic_src/**/*.{JPG,jpg}'
-### An example for PC could be 'C:/Users/username/Desktop/pic_src/**/*.{JPG,jpg}'
-pic_names = Dir['<source>']
+Dir.chdir 'C:/Users/trist/Desktop/pic_move_here'
+pic_names = Dir[C:/Users/trist/Desktop/pic_src/**/*.{JPG,jpg}]
 
 puts 'What would you like to call this batch?'
 batch_name = gets.chomp
 puts
 print "Downloading #{pic_names.length} files: "
-# This will be our counter. We'll start at 1 today,
-#  though normally I like to count from 0.
+
 pic_number = 1
 pic_names.each do |name|
-  print '.' # This is our "progress bar".
+  print '.'
   new_name = if pic_number < 10
     "#{batch_name}0#{pic_number}.jpg"
   else
     "#{batch_name}#{pic_number}.jpg"
   end
 
-  # Now where were we? Oh, yeah...
+  if File.exist? new_name
+
+  puts 'DANGER: file names already exist, exiting!'
+  exit
+  else
   File.rename name, new_name
-  # Finally, we increment the counter.
+  end
+
+  File.rename name, new_name
+
   pic_number += 1
 end
-puts # This is so we aren't on progress bar line.
-puts 'Done, cutie!'
+puts 
+puts 'Done'
