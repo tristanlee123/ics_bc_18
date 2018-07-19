587de2a78abc24b03c1adee737de8512887096f8
tristanlee123 <tristanasdf5809@gmail.com>
2018-07-18 20:23:06 -0700

    Complete progiling exercise

--- week6/ch14/profiling.rb
+++ week6/ch14/profiling.rb
@@ -1,6 +1,10 @@
 def profile block_description, &block
-  start_time = Time.new
-  block.call
-  duration = Time.new - start_time
-  puts "#{block_description}: #{duration} seconds"
-end
+  profiling_on = false
+  if profiling_on start_time = Time.new
+  	block.call
+  	duration = Time.new - start_time
+  	puts "#{block_description}: #{duration} seconds"
+  else
+  	block.call
+  end
+end 
